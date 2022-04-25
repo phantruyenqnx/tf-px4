Aer041/PX4-Autopilot

commander: Add prearm check for flight termination

src/modules/commander/Arming/PreFlightCheck/checks/preArmCheck.cpp
@@ -79,6 +79,12 @@ bool PreFlightCheck::preArmCheck(orb_advert_t *mavlink_log_pub, const vehicle_st
 		prearm_ok = false;
 	}
 
+	if (status_flags.condition_flight_terminated) {
+		if (report_fail) { mavlink_log_critical(mavlink_log_pub, "Arming denied! Flight termination active"); }
+
+		prearm_ok = false;
+	}
+
 	// USB not connected
 	if (!status_flags.circuit_breaker_engaged_usb_check && status_flags.usb_connected) {
 		if (report_fail) { mavlink_log_critical(mavlink_log_pub, "Arming denied! Flying with USB is not safe"); }

src/modules/commander/Commander.cpp
@@ -2823,6 +2823,8 @@ Commander::run()
 			checkWindAndWarn();
 		}
 
+		_status_flags.condition_flight_terminated = _armed.force_failsafe || _armed.lockdown || _armed.manual_lockdown;
+
 		/* Get current timestamp */
 		const hrt_abstime now = hrt_absolute_time();
 

phantruyenqnx/tf-px4

src/modules/commander/Arming/PreFlightCheck/checks/preArmCheck.cpp
@@ -79,6 +79,12 @@ bool PreFlightCheck::preArmCheck(orb_advert_t *mavlink_log_pub, const vehicle_st
 		prearm_ok = false;
 	}
 
+	if (status_flags.flight_terminated) {
+		if (report_fail) { mavlink_log_critical(mavlink_log_pub, "Arming denied! Flight termination active"); }
+
+		prearm_ok = false;
+	}
+
 	// USB not connected
 	if (!status_flags.circuit_breaker_engaged_usb_check && status_flags.usb_connected) {
 		if (report_fail) { mavlink_log_critical(mavlink_log_pub, "Arming denied! Flying with USB is not safe"); }

src/modules/commander/Commander.cpp
@@ -2911,6 +2911,8 @@ Commander::run()
 			checkWindAndWarn();
 		}
 
+		_status_flags.flight_terminated = _armed.force_failsafe || _armed.lockdown || _armed.manual_lockdown;
+
 		/* Get current timestamp */
 		const hrt_abstime now = hrt_absolute_time();
 