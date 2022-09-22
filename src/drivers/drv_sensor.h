PX4/PX4-Autopilot

drivers: add support for ICP101XX and ICP201XX

src/drivers/drv_sensor.h
@@ -208,6 +208,9 @@
 
 #define DRV_HYGRO_DEVTYPE_SHT3X 0xB1
 
+#define DRV_BARO_DEVTYPE_ICP101XX 0xB2
+#define DRV_BARO_DEVTYPE_ICP201XX 0xB3
+
 #define DRV_DEVTYPE_UNUSED		0xff
 
 #endif /* _DRV_SENSOR_H */

phantruyenqnx/tf-px4

src/drivers/drv_sensor.h
@@ -216,8 +216,8 @@
 #define DRV_FLOW_DEVTYPE_PX4FLOW 0xB5
 #define DRV_FLOW_DEVTYPE_PAA3905 0xB6
 
-#define DRV_BARO_DEVTYPE_ICP10100 0xC0
-#define DRV_BARO_DEVTYPE_ICP10111 0xC1
+#define DRV_BARO_DEVTYPE_ICP101XX 0xB7
+#define DRV_BARO_DEVTYPE_ICP201XX 0xB8
 
 #define DRV_DEVTYPE_UNUSED		0xff
 